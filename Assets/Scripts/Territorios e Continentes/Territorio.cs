92266102966928690f250d7a9585df3128e62e70
Allrthur <arthurbittencourt2@gmail.com>
2021-07-29 22:46:09 -0300

    Prefab feito
    falta o codigo

--- Assets/Scripts/Territorios e Continentes/Territorio.cs
+++ Assets/Scripts/Territorios e Continentes/Territorio.cs
@@ -6,26 +6,28 @@ namespace FormigaWar.Territorios
 {
     public class Territorio : MonoBehaviour
     {
-        private string nome;
-        private List<Fronteira> fronteiras;
-        private Carta cartaterritorio;
-        private int numtropas;
-        private Continente continente;
+        [SerializeField] private TextMesh numtropas_txt;
 
-        public Territorio(string nome, List<Fronteira> fronteiras, Carta cartaterritorio, int numtropas, Continente continente) // construtor basico
-        {
-            this.nome = nome;
-            this.fronteiras = fronteiras;
-            this.cartaterritorio = cartaterritorio;
-            this.numtropas = numtropas;
-            this.continente = continente; // talvez colocar isso na hora de fazer o continente?
-        }
+        [SerializeField]private string nome;
+        [SerializeField]private List<Fronteira> fronteiras;
+        [SerializeField]private Carta cartaterritorio;
+        [SerializeField]private int numtropas;
+        [SerializeField]private Continente continente;
+
+        //public Territorio(string nome, List<Fronteira> fronteiras, Carta cartaterritorio, int numtropas, Continente continente) // construtor basico
+        //{
+        //    this.nome = nome;
+        //    this.fronteiras = fronteiras;
+        //    this.cartaterritorio = cartaterritorio;
+        //    this.numtropas = numtropas;
+        //    this.continente = continente; // talvez colocar isso na hora de fazer o continente?
+        //}
 
         // Setters
         public void SetNome(string nome) { this.nome = nome; }
         public void SetFronteiras(List<Fronteira> fronteiras) { this.fronteiras = fronteiras; }
         public void SetCarta(Carta cartaterritorio) { this.cartaterritorio = cartaterritorio; }
-        public void SetNumTropas(int numtropas) { this.numtropas = numtropas; }
+        public void SetNumTropas(int numtropas) { this.numtropas = numtropas; AtualizarNumTropas(); }
         public void SetContinente(Continente continente) { this.continente = continente; }
 
         // Getters
@@ -35,5 +37,15 @@ namespace FormigaWar.Territorios
         public int GetNumTropas() { return this.numtropas; }
         public Continente GetContinente() { return this.continente; }
 
+
+        void Start() 
+        {
+            numtropas_txt.text = numtropas.ToString();
+        }
+
+        private void AtualizarNumTropas()
+        {
+            numtropas_txt.text = numtropas.ToString();
+        }
     }
 }
