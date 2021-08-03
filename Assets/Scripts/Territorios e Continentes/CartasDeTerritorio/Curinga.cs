8047c2bbb5bbda277f00cc71013ec173941ecff8
MarcosFRHayman <marcoshayman@id.uff.br>
2021-08-03 11:04:33 -0300

    Remove atributo CreateAssetMenu de Curinga
    Ele era um ScriptableObject e por isso tinha esse atributo, mas como não é mais, ele deveria ter o perdido, porém esqueci no último commit

--- Assets/Scripts/Territorios e Continentes/CartasDeTerritorio/Curinga.cs
+++ Assets/Scripts/Territorios e Continentes/CartasDeTerritorio/Curinga.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace FormigaWar.Territorios
 {
-    [CreateAssetMenu(menuName = "ScriptableObjects/CartaCuringa", fileName = "newCuringa")]
     public class Curinga : Carta
     {
         public bool TemSimbolo(Simbolo simbolo) => true;
