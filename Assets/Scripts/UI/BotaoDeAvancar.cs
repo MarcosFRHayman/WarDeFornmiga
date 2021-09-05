802334aaff11690730f2fef7c5a520cd1db2a939
juanpmf13 <juanpablomonteirofernandes@id.uff.br>
2021-09-05 20:05:55 -0300

    Botão de avançar

---
+++ Assets/Scripts/UI/BotaoDeAvancar.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FormigaWar
+{
+    public class BotaoDeAvancar : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public Button avancar;
+
+        private void Start()
+        {
+            avancar.onClick.AddListener(Taskavanca);
+        }
+
+        private void Taskavanca()
+        {
+            TurnoManager.faseAtual += 1;
+            if (TurnoManager.faseAtual>=4)
+            {
+                TurnoManager.faseAtual = 0;
+                TurnoManager.jogadorDaVez += 1;
+                Debug.Log("final!!!!!!");
+                Debug.Log(TurnoManager.faseAtual);
+                Debug.Log(TurnoManager.jogadorDaVez);
+                if (TurnoManager.jogadorDaVez>= TurnoManager.jogadoresNaMesa.Length)
+                {
+                    TurnoManager.jogadorDaVez = 0;
+                }
+            }
+            Debug.Log(TurnoManager.faseAtual);
+            Debug.Log(TurnoManager.jogadorDaVez);
+        }
+    }
+}
